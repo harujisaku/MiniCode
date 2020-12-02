5dc7af55845d88130411008950e2b1646cada8dc
harujisaku <64310155+harujisaku@users.noreply.github.com>
2020-12-02 21:23:03 +0900

    add highlight and java highlight

---
+++ src/harujisaku/minicode/highlight/JavaHighlight.java
@@ -0,0 +1,5 @@
+package harujisaku.minicode.highlight;
+
+class JavaHighlight implements Highlight {
+	String[] MAGENTA_KEYWORDS = {"abstract","assert","break","case","catch","class","const","continue","default","do","else","enum","extends","final","finally","for","goto","if","implements","import","instanceof","native","new","package","private"."private","public","return","static","strictfp","switch","synchronized","throw","ththrows","transient","try","void","volatile","while",}
+}
