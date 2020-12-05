3ba0cf979b47d1182ea0063df02d064c416af6d2
harujisaku <64310155+harujisaku@users.noreply.github.com>
2020-12-06 06:16:18 +0900

    add =,==,>=,<=,|,||,&and&& in magenta syntax highlight

--- src/harujisaku/minicode/highlight/Highlight.java
+++ src/harujisaku/minicode/highlight/Highlight.java
@@ -61,6 +61,7 @@ public class Highlight{
 		}
 		buff.deleteCharAt(buff.length() - 1);
 		buff.append(")");
+		System.out.println(buff.toString());
 		return buff.toString();
 	}
 	
--- src/harujisaku/minicode/highlight/JavaHighlight.java
+++ src/harujisaku/minicode/highlight/JavaHighlight.java
@@ -3,17 +3,21 @@ package harujisaku.minicode.highlight;
 import java.awt.Color;
 
 public class JavaHighlight extends AbstractHighlight {
-	String [] MAGENTA_KEYWORDS = {"abstract","assert","break","case","catch","class","const","continue","default","do","else","enum","extends","final","finally","for","goto","if","implements","import","instanceof","native","new","package","private","private","public","return","static","strictfp","switch","synchronized","throw","ththrows","transient","try","void","volatile","while"};
+	String WHITE_SPACE_REGEX = "[ /\\\\(\\)\"':,.;~\\!@#$%\\^\\[\\]\\{\\}`\\?\\-…\\t]";
+	String [] MAGENTA_KEYWORDS = {"abstract","assert","break","case","catch","class","const","continue","default","do","else","enum","extends","final","finally","for","goto","if","implements","import","instanceof","native","new","package","private","private","public","return","static","strictfp","switch","synchronized","throw","ththrows","transient","try","void","volatile","while","\\|","\\|\\|","\\&","=","\\>","\\<","==",">=","<=","&&"};
 	String[] CYAN_KEYWORDS = {WHITE_SPACE_REGEX+"+[a-z]+[a-zA-Z0-9]+\\("};
-	String[] YELLOW_KEYWORDS = {WHITE_SPACE_REGEX+"[A-Z]{1,2}[a-z][a-zA-Z0-9]+(?="+WHITE_SPACE_REGEX+")(?!\\()"};
+	String[] YELLOW_KEYWORDS = {WHITE_SPACE_REGEX+"([A-Z]{1,2}[a-z][a-zA-Z0-9]+|int|char|boolean|void|Override|override)(?="+WHITE_SPACE_REGEX+")(?!\\()"};
 	String[] GREEN_KEYWORDS = {"\".*\""};
-	String[] ORANGE_KEYWORDS = {"[A-Z]+[A-Z_]"};
+	String[] ORANGE_KEYWORDS = {"([A-Z]+[A-Z_]|null|true|false"+WHITE_SPACE_REGEX+"[0-9]+"+WHITE_SPACE_REGEX+")"};
+	String[] RED_KEYWORDS = {WHITE_SPACE_REGEX+"(super|this)"+WHITE_SPACE_REGEX};
 	public JavaHighlight(){
 		super.MAGENTA_KEYWORDS=this.MAGENTA_KEYWORDS;
 		super.CYAN_KEYWORDS=this.CYAN_KEYWORDS;
 		super.YELLOW_KEYWORDS=this.YELLOW_KEYWORDS;
 		super.GREEN_KEYWORDS=this.GREEN_KEYWORDS;
 		super.ORANGE_KEYWORDS=this.ORANGE_KEYWORDS;
+		super.RED_KEYWORDS=this.RED_KEYWORDS;
+		super.WHITE_SPACE_REGEX=this.WHITE_SPACE_REGEX;
 	}
 	
 	@Override
