fe246b457a38afe19759d4079698fc601d72045f
harujisaku <64310155+harujisaku@users.noreply.github.com>
2020-12-12 07:22:14 +0900

    update change codePanel to list

--- src/harujisaku/minicode/panel/MainPanel.java
+++ src/harujisaku/minicode/panel/MainPanel.java
@@ -3,19 +3,24 @@ package harujisaku.minicode.panel;
 import harujisaku.minicode.*;
 import harujisaku.minicode.panel.*;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import java.awt.BorderLayout;
 
 public class MainPanel extends JPanel {
+	List<JCodePanel> codePanelList = new ArrayList<JCodePanel>();
 	public MainPanel(){
 		setLayout(new BorderLayout());
 		JConsolePanel console = new JConsolePanel();
-		JCodePanel code = new JCodePanel(4);
+		codePanelList.add(new JCodePanel(4));
 		JSplitPane splitpane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
 		splitpane.setResizeWeight(1.0);
 		splitpane.setBottomComponent(console);
-		splitpane.setTopComponent(code);
+		splitpane.setTopComponent(codePanelList.get(0));
 		add(splitpane,BorderLayout.CENTER);
 		splitpane.setDividerLocation(-1);
 	}
