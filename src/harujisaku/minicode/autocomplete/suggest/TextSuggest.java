55d34592c1be26ed7501dae083b7d0408e036ce6
harujisaku <64310155+harujisaku@users.noreply.github.com>
2020-12-13 21:33:21 +0900

    add text highlight and autosuggest

---
+++ src/harujisaku/minicode/autocomplete/suggest/TextSuggest.java
@@ -0,0 +1,8 @@
+package harujisaku.minicode.autocomplete.suggest;
+/**
+* 通常のテキストの自動補完設定クラスです.
+*/
+
+public class TextSuggest extends AbstractSuggest {
+	
+}
---
+++ src/harujisaku/minicode/highlight/TextHighlight.java
@@ -0,0 +1,9 @@
+package harujisaku.minicode.highlight;
+
+/**
+* 通常のテキストのシンタックスハイライトの設定クラスです.
+*/
+
+public class TextHighlight{
+	
+}
